8223f62b9a70a2a55a7ff3f9c3dcde8ffdca112f
MarioCaudillo <110208641+MarioCaudillo@users.noreply.github.com>
2024-04-22 01:41:09 -0600

    bd modified

--- mazacauata_bd.sql
+++ mazacauata_bd.sql
@@ -1,75 +1,141 @@
-CREATE TABLE venta (
-    id_venta INT AUTO_INCREMENT PRIMARY KEY,
-    id_usuario INT,
-    isbn VARCHAR(100),
-    fecha_venta DATE NOT NULL,
-    cantidad_vendida INT NOT NULL,
-    precio_venta DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
-    FOREIGN KEY (isbn) REFERENCES libro(isbn),
-    CONSTRAINT fk_tipo_usuario
-        FOREIGN KEY (id_usuario)
-        REFERENCES usuario(id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE
-);
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: mazacuatastore
+-- ------------------------------------------------------
+-- Server version	8.0.27
 
-CREATE TABLE compra (
-    ID_compra INT AUTO_INCREMENT PRIMARY KEY,
-    id_usuario INT,
-    isbn VARCHAR(100),
-    fecha_compra DATE NOT NULL,
-    cantidad INT NOT NULL,
-    precio_total DECIMAL(10, 2) NOT NULL,
-    estado_compra ENUM('pendiente', 'completada', 'cancelada') NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
-    FOREIGN KEY (isbn) REFERENCES libro(isbn),
-    CONSTRAINT fk_usuario_compra
-        FOREIGN KEY (id_usuario)
-        REFERENCES usuario(id)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE ticket (
-    id_ticket INT AUTO_INCREMENT PRIMARY KEY,
-    id_compra INT,
-    isbn VARCHAR(100),
-    cantidad INT NOT NULL,
-    precio_venta DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (id_compra) REFERENCES compra(ID_compra),
-    FOREIGN KEY (isbn) REFERENCES libro(isbn)
-);
-ALTER TABLE usuario ADD COLUMN password VARCHAR(255) NOT NULL;
-ALTER TABLE usuario
-MODIFY COLUMN password VARCHAR(255) NOT NULL AFTER email;
-ALTER TABLE usuario
-ADD COLUMN direccion VARCHAR(255) AFTER tipo_usuario;
+--
+-- Table structure for table `compra`
+--
 
--- dummy data: tabla usuario
-INSERT INTO usuario (nombre, apellidos, email, password, tipo_usuario, direccion, rfc, created_at, updated_at)
-VALUES 
-    ('Juan', 'Perez', 'juan@example.com', 'password123', 'vendendor', 'Calle 123', 'ABCD123456', NOW(), NOW()),
-    ('Maria', 'Gomez', 'maria@example.com', 'password456', 'usuario_online', 'Avenida 456', NULL, NOW(), NOW()),
-    ('Carlos', 'Lopez', 'carlos@example.com', 'password789', 'admin', 'Plaza 789', 'EFGH987654', NOW(), NOW());
+DROP TABLE IF EXISTS `compra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compra` (
+  `ID_compra` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int DEFAULT NULL,
+  `isbn` varchar(100) DEFAULT NULL,
+  `fecha_compra` date NOT NULL,
+  `cantidad` int NOT NULL,
+  `precio_total` decimal(10,2) NOT NULL,
+  `estado_compra` enum('pendiente','completada','cancelada') NOT NULL,
+  PRIMARY KEY (`ID_compra`),
+  KEY `isbn` (`isbn`),
+  KEY `fk_usuario_compra` (`id_usuario`),
+  CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `compra_ibfk_2` FOREIGN KEY (`isbn`) REFERENCES `libro` (`isbn`),
+  CONSTRAINT `fk_usuario_compra` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO libro (isbn, titulo, autor, editorial, edición, precio, calificacion, portada, unidades_disponibles, ubicacion, genero)
-VALUES 
-    ('9788498389085', 'Cien años de soledad', 'Gabriel García Márquez', 'Editorial Sudamericana', '1', '20.00', '4.5', 'https://example.com/portada1.jpg', 10, 'Sección A', 'Ficción'),
-    ('9788437610786', 'El principito', 'Antoine de Saint-Exupéry', 'Salamandra', '1', '15.00', '4.7', 'https://example.com/portada2.jpg', 15, 'Sección B', 'Infantil'),
-    ('9788408105474', 'El código Da Vinci', 'Dan Brown', 'Planeta', '1', '25.00', '4.2', 'https://example.com/portada3.jpg', 20, 'Sección C', 'Misterio');
-    
-INSERT INTO compra (id_usuario, isbn, fecha_compra, cantidad, precio_total, estado_compra)
-VALUES 
-    (1, '9788498389085', '2024-04-21', 2, 40.00, 'completada'),
-    (2, '9788437610786', '2024-04-20', 1, 15.00, 'completada'),
-    (3, '9788408105474', '2024-04-19', 3, 75.00, 'pendiente');
-    
-INSERT INTO ticket (id_compra, isbn, cantidad, precio_venta)
-VALUES 
-    (13, '9788498389085', 2, 40.00),
-    (14, '9788437610786', 1, 15.00),
-    (15, '9788408105474', 3, 75.00);
-    
-select * from compra;
-    
+--
+-- Table structure for table `libro`
+--
+
+DROP TABLE IF EXISTS `libro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `libro` (
+  `isbn` varchar(100) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `autor` varchar(255) NOT NULL,
+  `editorial` varchar(255) NOT NULL,
+  `edición` varchar(255) DEFAULT NULL,
+  `precio` float NOT NULL,
+  `calificacion` float DEFAULT NULL,
+  `portada` varchar(255) DEFAULT NULL,
+  `unidades_disponibles` int NOT NULL,
+  `ubicacion` varchar(255) DEFAULT NULL,
+  `genero` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`isbn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ticket`
+--
+
+DROP TABLE IF EXISTS `ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticket` (
+  `id_ticket` int NOT NULL AUTO_INCREMENT,
+  `id_compra` int DEFAULT NULL,
+  `isbn` varchar(100) DEFAULT NULL,
+  `cantidad` int NOT NULL,
+  `precio_venta` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id_ticket`),
+  KEY `id_compra` (`id_compra`),
+  KEY `isbn` (`isbn`),
+  CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`id_compra`) REFERENCES `compra` (`ID_compra`),
+  CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`isbn`) REFERENCES `libro` (`isbn`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `tipo_usuario` enum('vendendor','usuario_online','admin') DEFAULT NULL,
+  `direccion` varchar(255) DEFAULT NULL,
+  `dirección` varchar(255) DEFAULT NULL,
+  `rfc` varchar(13) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `venta`
+--
+
+DROP TABLE IF EXISTS `venta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `venta` (
+  `id_venta` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int DEFAULT NULL,
+  `isbn` varchar(100) DEFAULT NULL,
+  `fecha_venta` date NOT NULL,
+  `cantidad_vendida` int NOT NULL,
+  `precio_venta` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id_venta`),
+  KEY `isbn` (`isbn`),
+  KEY `fk_tipo_usuario` (`id_usuario`),
+  CONSTRAINT `fk_tipo_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`),
+  CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`isbn`) REFERENCES `libro` (`isbn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-22  1:33:54
