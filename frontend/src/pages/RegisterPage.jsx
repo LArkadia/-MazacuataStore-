06118ee5075fb12ec8afe0416b69e96308686ddf
Cristopher Itou <108854506+TheApexPredatorp@users.noreply.github.com>
2024-06-07 13:47:58 -0600

    Xd

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,6 +1,21 @@
 function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+    <div>
+      <h3 className="text-2xl font-bold">Register</h3>
+
+      <form>
+        <input type="text" placeholder="Ingresa tu nombre"
+          className="mb-2 bg-zinc-500 px-2 py-1 rounded-sm"
+        />
+        <input type="text" placeholder="Ingresa tu apellido"/>
+        <input type="text" placeholder="Ingresa tu dirección"/>
+        <input type="email" placeholder="Ingresa tu correo eléctronico"/>
+        <input type="password" placeholder="Ingresa tu constraseña"/>
+        <button>
+          Registrate
+        </button>
+      </form>
+    </div>
   )
 }
 
