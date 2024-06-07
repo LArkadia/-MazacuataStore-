b87e4e604cc924814277d8d9e8cf4fe3525387eb
Cristopher Itou <108854506+TheApexPredatorp@users.noreply.github.com>
2024-06-07 15:23:10 -0600

    Avance del Registro

--- frontend/src/components/ui/Input.jsx
+++ frontend/src/components/ui/Input.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export function Input(props) {
   return (
-    <input type="text" 
-    className="mb-2 bg-white px-2 py-1 rounded-lg italic text-gray-500 border border-gray-500"
+    <input 
+    type="text" 
+    className="mb-2 bg-white px-3 py-2 block my-2 w-full rounded-lg italic text-gray-500 border border-gray-500"
     {...props}
     />
   )
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,19 +1,20 @@
 import {Input} from '../components/ui/Input'
+import { Card } from "../components/ui/Card";
 
 function RegisterPage() {
   return (
-    <div>
-       <h3 className="text-2xl font-bold">Crea tu cuenta</h3>
-      <form>
-        <Input placeholder="Ingresa tu nombre"/>
-        <input type="text" placeholder="Ingresa tu apellido"/>
-        <input type="text" placeholder="Ingresa tu dirección"/>
-        <input type="email" placeholder="Ingresa tu correo eléctronico"/>
-        <input type="password" placeholder="Ingresa tu constraseña"/>
-        <button>
-          Registrate
-        </button>
-      </form>
+    <div className='h-[calc(100vh-64px)] flex items-center justify-center'>
+      <Card>
+        <h3 className="text-2xl font-bold">Crea tu cuenta</h3>
+        <form>
+          <Input placeholder="Ingresa tu nombre"/>
+          <Input placeholder="Ingresa tu apellido"/>
+          <Input placeholder="Ingresa tu dirección"/>
+          <button>
+            Registrate
+          </button>
+        </form>
+      </Card>
     </div>
   )
 }
