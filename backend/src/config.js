cd873a43dfbebfe8d12035788d925b44ae77effb
Cristopher Itou <108854506+TheApexPredatorp@users.noreply.github.com>
2024-06-10 20:41:51 -0600

    mrtetas

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,7 +7,7 @@ module.exports = {
     mysql:{
         host:   process.env.MYSQL_HOST || 'localhost',
         user:   process.env.MYSQL_USER || 'root',
-        password:   process.env.MYSQL_PASSWORD || 'mrtetas',
+        password:   process.env.MYSQL_PASSWORD || '',
         database:   process.env.MYSQL_DB || 'mazacuatastore' 
     }
 };
