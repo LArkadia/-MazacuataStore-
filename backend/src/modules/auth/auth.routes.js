e0c615fce28dd8fd8e314a12df59a0013022f4a9
Cristopher Itou <108854506+TheApexPredatorp@users.noreply.github.com>
2024-06-18 15:51:06 -0600

    Get Token in Login
    Se modifico la API auth.rotes para recibir metodo Post y el Login ahora recibe un Token, falta almacenarlo en una cookie para las rutas

--- backend/src/modules/auth/auth.routes.js
+++ backend/src/modules/auth/auth.routes.js
@@ -1,20 +1,20 @@
 const express = require('express');
 
 const answer = require('../../network/answers');
-const controller    =   require('./index');
+const controller = require('./index');
 
 const router = express.Router();
 
-router.get('/login', login);
+router.post('/login', login);
 
 async function login(req, res, next) {
     try {
-        const token =   await controller.login(req.body.email, req.body.password);
+        const { email, password } = req.body;
+        const token = await controller.login(email, password);
         answer.success(req, res, token, 200);
     } catch (error) {
-        next(error)
+        next(error);
     }
 }
 
-
-module.exports  =   router;
+module.exports = router;
