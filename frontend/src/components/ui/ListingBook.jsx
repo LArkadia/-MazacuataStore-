cb1f693d312f9bf278635d0f162d379f91f6a82a
Alfredo Bautista <alfredobr587@gmail.com>
2024-06-28 03:24:28 -0600

    Added stock line

--- frontend/src/components/ui/ListingBook.jsx
+++ frontend/src/components/ui/ListingBook.jsx
@@ -7,7 +7,6 @@ export const ListingBook = (content,index=1,isHome=false) => {
   return (
     <div key={index} className="bg-white rounded-lg shadow-md overflow-hidden">
                 <div className="aspect-w-1 aspect-h-1">
-                  {/* Aquí irá la imagen del libro */}
                   <Link to={`http://localhost:5173/books/single?title=${data.titulo}`}>
                     <img 
                       src={data.portada} // Direccion o ubicación de portada
@@ -18,6 +17,7 @@ export const ListingBook = (content,index=1,isHome=false) => {
                 </div>
                 <div className="p-4">
                   <h3 className="text-lg font-medium">{data.titulo}</h3>
+                  <p className="text-gray-600 ">Disponibles: {data.unidades_disponibles}</p>
                   <p className="text-gray-600 "><i class="fa-solid fa-money-bill"></i> ${data.precio}</p>
                 </div>
     </div>
