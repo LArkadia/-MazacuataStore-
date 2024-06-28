b5f6437d9623e477d69a7e83e1e49899b32962bb
Angel788 <angel-yahel@outlook.com>
2024-06-27 23:58:51 -0600

    Fixing Links

--- frontend/src/components/ui/ListingBook.jsx
+++ frontend/src/components/ui/ListingBook.jsx
@@ -8,7 +8,7 @@ export const ListingBook = (content,index=1,isHome=false) => {
     <div key={index} className="bg-white rounded-lg shadow-md overflow-hidden">
                 <div className="aspect-w-1 aspect-h-1">
                   {/* Aquí irá la imagen del libro */}
-                  <Link to={`http://localhost:5173/books/single?isbn=${data.isbn}`}>
+                  <Link to={`http://localhost:5173/books/single?title=${data.titulo}`}>
                     <img 
                       src={data.portada} // Direccion o ubicación de portada
                       alt={data.titulo}
