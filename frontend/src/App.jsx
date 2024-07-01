387b72e9a8478f5888e7f4fe02fbc244da663e9d
Cristopher Itou <108854506+TheApexPredatorp@users.noreply.github.com>
2024-06-30 22:37:03 -0600

    Admins Y no Admins

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,29 +15,35 @@ import CRUD from "./pages/CRUD";
 import { MianContent } from './components/MianContent';
 import NotFound from './pages/NotFound';
 
+import ProtectedRoute from './components/ProtectedRoute';
 
-const routes=createBrowserRouter(
+const routes = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<MianContent/>} >
-      <Route index element={<HomePage/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path='/register' element={<RegisterPage/>}/>
-      <Route path='/books/single' element={<BookFormPage/>}/>
-      <Route path='/books' element={<BookPage/>}/>
-      <Route path='/profile' element={<ProfilePage/>}/>
-      <Route path='/cart' element={<ShopCartPage/>}/>
-      <Route path='/ticket' element={<TicketPage/>}/>
-      <Route path='/scanner' element={<Scanner/>}/>
-      <Route path='/puntodeventa' element={<POS/>}/>
-      <Route path='/payment' element={<Pago/>}/>
-      <Route path='/stats' element={<Stats/>}/>
-      <Route path='/crud' element={<CRUD/>}/>
-      <Route path="*" element={<NotFound/>}/>
+    <Route path='/' element={<MianContent />}>
+      <Route index element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      <Route path='/books/single' element={<BookFormPage />} />
+      <Route path='/books' element={<BookPage />} />
+      <Route path='/profile' element={<ProfilePage />} />
+      <Route path='/cart' element={<ShopCartPage />} />
+      <Route path='/ticket' element={<TicketPage />} />
+      <Route path='/scanner' element={<Scanner />} />
+      <Route path='/puntodeventa' element={<POS />} />
+      <Route path='/payment' element={<Pago />} />
+      <Route path='/stats' element={<Stats />} />
+      <Route path='/crud' element={
+        <ProtectedRoute adminOnly>
+          <CRUD />
+        </ProtectedRoute>
+      } />
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
+
 function App() {
-  return <RouterProvider router={routes}/>;
+  return <RouterProvider router={routes} />;
 }
 
-export default App
+export default App;
